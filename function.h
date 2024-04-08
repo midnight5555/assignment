9a3efecb3d45df6ad4ed828ff8ef4b92e05ef97a
kanana <166382712+midnight5555@users.noreply.github.com>
2024-04-09 01:26:14 +0300

    Update function.h

--- function.h
+++ function.h
@@ -1,2 +1,13 @@
-void populate_patient();
-void print_patient();
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+struct patients
+{ 
+	char name[50];
+	char doctor[50];
+
+};
+void populatepatient(struct patients []);
+void printpatient( struct patients[]);
+
+#endif
